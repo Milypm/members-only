fcfafa82ba4195e4d20d73f8d6f477b1105cd5ec
milypm <mp_morones@hotmail.com>
2021-02-03 16:43:36 -0600

    Edit posts/index view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -66,6 +66,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:title, :description, :user_id)
+      params.require(:post).permit(:title, :description, :name, :user_id)
     end
 end
